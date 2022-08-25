a5453de36c803ad44bd33e6128403e9834c4e33e
OziOcb <pozyzniewski@yahoo.com>
2022-08-25 15:29:35 +0100

    Before - Gas Optimizations using storage knowledge - 11:52:38

--- hardhat.config.js
+++ hardhat.config.js
@@ -33,11 +33,11 @@ module.exports = {
     },
   },
   gasReporter: {
-    enabled: false,
+    enabled: true,
     outputFile: "gas-report.txt",
     noColors: true,
     currency: "USD",
-    coinmarketcap: COINMARKETCAP_API_KEY,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
     // token: "Matic",
   },
   etherscan: {
